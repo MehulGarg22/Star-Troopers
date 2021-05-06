0e9a83f66c96c40016c6d919c6ffd79bf3f5bd0e
unknown <mehul.garg104@gmail.com>
2021-05-06 20:31:02 +0530

    Update

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Card=({name, email, username})=>
 {
  return(
-    <div className="bg-white dib br3 pa3 bw2 shadow-5 ma2 grow">
+    <div className="bg-white dib br3 pa2 bw2 shadow-5 ma2 grow">
          <img alt="robots" src={`https://robohash.org/${name}?size=200x200`} />
         <div>
            <h1>{name}</h1>
--- src/components/Scroll.js
+++ src/components/Scroll.js
@@ -1,11 +1,9 @@
 import React from 'react';
 
 const Scroll=(props)=>{
-  return (<div className="bg-light-green dib br3 pa3 bw2 shadow-5 ma2" style={{overflowY:'scroll', height: '500px' }}>
+  return (<div className="bg-light-green dib br3 pa3 bw2 shadow-5 ma2" style={{overflowY:'scroll', height: '457px' }}>
        {props.children}
    </div>);
 }
-
-
 
 export default Scroll;
